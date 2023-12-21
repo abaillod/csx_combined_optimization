8c03f2b08bf85fefc43817b5bb7c2192b2d69ef1
Antoine Baillod <ab5667@columbia.edu>
2023-12-21 14:26:05 -0500

    update input generator

--- inputs/generate_inputs.py
+++ inputs/generate_inputs.py
@@ -6,8 +6,10 @@ import pickle
 
 inputs = single_stage_cnt32.inputs
 
-inputs['vmec']['dofs']['mpol'] = 1
-inputs['vmec']['dofs']['ntor'] = 1
+res = 3
+
+inputs['vmec']['dofs']['internal_mpol'] = res
+inputs['vmec']['dofs']['internal_ntor'] = res
 
 
 coil_weight = np.linspace(1, 1e2, 5)
@@ -17,12 +19,15 @@ counter=0
 for cw in coil_weight:
     for ccw in coil_constraint_weight:
         counter+=1
-        inputs['directory'] = f'runs/cnt32_improve_qs_mpol=ntor=1_cweight={cw}_wcurve={ccw}'
+        inputs['directory'] = f'runs/cnt32_improve_qs_mpol=ntor={res}_cweight={cw}_wcurve={ccw}'
         inputs['coils_objective_weight'] = Weight(cw)
-        inputs['cnt_coils']['target']['IL_length_weight'] = Weight(ccw)
-        inputs['cnt_coils']['target']['IL_msc_weight'] = Weight(ccw)
-        inputs['cnt_coils']['target']['IL_maxc_weight'] = Weight(ccw)
-        inputs['cnt_coils']['target']['PF_current_weight'] = Weight(ccw)
+        inputs['cnt_coils']['target']['IL_length_weight'] = Weight(10**(-ccw))
+        inputs['cnt_coils']['target']['IL_msc_weight'] = Weight(10**(-ccw))
+        inputs['cnt_coils']['target']['IL_maxc_weight'] = Weight(10**(-ccw))
+        inputs['cnt_coils']['target']['PF_current_weight'] = Weight(10**(-ccw))
 
-        with open(os.path.join('scan_mpol=ntor=1', f'cnt32_{counter}.pckl'), 'wb') as f:
+        inputs['vmec']['filename'] = f'runs/cnt32_improve_qs_mpol=ntor={res-1}_cweight={cw}_wcurve={ccw}/input.final'
+        inputs['cnt_coils']['geometry']['filename'] = f'cnt32_improve_qs_mpol=ntor={res-1}_cweight={cw}_wcurve={ccw}/coils/bs_output.json'
+
+        with open(os.path.join(f'scan_mpol=ntor={res}', f'cnt32_{counter}.pckl'), 'wb') as f:
             pickle.dump(inputs, f)
