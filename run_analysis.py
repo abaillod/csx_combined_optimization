68a27effdac087bb1495a7d35926237e9b793749
Antoine Baillod <antoine.baillod@epfl.ch>
2024-02-28 10:00:26 -0500

    updated windowpane sets

--- run_analysis.py
+++ run_analysis.py
@@ -4,13 +4,14 @@ import argparse
 import os
 from simsopt._core.optimizable import load, save
 from simsopt.geo.surfacerzfourier import SurfaceRZFourier
+from simsopt.field import BiotSavart
 import numpy as np
 import matplotlib.pyplot as plt
 from simsopt.field import SurfaceClassifier, \
     particles_to_vtk, compute_fieldlines, LevelsetStoppingCriterion, plot_poincare_data
 from simsopt.geo import CurveLength
 import pickle
-
+from pystellplot.VMEC.plot_surfaces import plot_vmec_surfaces
 
 # Read command line arguments
 parser = argparse.ArgumentParser()
@@ -210,6 +211,64 @@ def trace_fieldlines(bfield,label):
     return fieldlines_phi_hits
 
 hits = trace_fieldlines(bs, 'vmec')
+
+
+# Plot without WPs
+bs = load(os.path.join(this_path, 'coils/bs_output.json'))
+coils = bs.coils
+if len(coils)>4:
+    coils = coils[:4]
+    bs = BiotSavart( coils )
+
+    surf = v.boundary
+    theta = surf.quadpoints_theta
+    phi = surf.quadpoints_phi
+    ntheta = theta.size
+    nphi = phi.size
+    bs.set_points(surf.gamma().reshape((-1,3)))
+    Bdotn = np.sum(bs.B().reshape((nphi, ntheta, 3)) * surf.unitnormal(), axis=2)
+    modB = bs.AbsB().reshape((nphi,ntheta))
+    
+    fig, ax = plt.subplots(figsize=(12,5))
+    c = ax.contourf(theta,phi,Bdotn / modB)
+    plt.colorbar(c)
+    ax.set_title(r'$\mathbf{B}\cdot\hat{n} / |B|$ ')
+    ax.set_ylabel(r'$\phi$')
+    ax.set_xlabel(r'$\theta$')
+    plt.savefig(os.path.join(figure_path, 'normal_field_error_no_wp.png'))
+
+
+    
+    # Run and plot Poincare section
+    vmec_surf = SurfaceRZFourier.from_wout(v.output_file) # vmec surface
+    vmec_surf_1 = SurfaceRZFourier.from_wout(v.output_file) # Expanded vmec surface
+    vmec_surf_1.extend_via_normal(0.1)
+    nfp = vmec_surf.nfp
+    
+    Rmaj = vmec_surf.major_radius()
+    r0 = vmec_surf.minor_radius()
+    sc_fieldline = SurfaceClassifier(vmec_surf_1, h=0.01, p=3)
+    nfieldlines = 50
+    tmax_fl = 2500
+    degree = 4
+    
+    def trace_fieldlines(bfield,label):
+        # Set up initial conditions - 
+        R0 = np.linspace(Rmaj-2*r0, Rmaj+2*r0, nfieldlines)
+        Z0 = np.zeros(nfieldlines)
+        phis = [(i/4)*(2*np.pi/nfp) for i in range(4)]
+        fieldlines_tys, fieldlines_phi_hits = compute_fieldlines(
+            bfield, R0, Z0, tmax=tmax_fl, tol=1e-8,
+            phis=phis, stopping_criteria=[LevelsetStoppingCriterion(sc_fieldline.dist)])
+        plot_poincare_data(fieldlines_phi_hits, phis, os.path.join(figure_path, 'poincare_no_wp'), dpi=150,surf=vmec_surf,mark_lost=True)
+        return fieldlines_phi_hits
+    
+    hits = trace_fieldlines(bs, 'vmec')
+
+
+
+
+
 
 
 
